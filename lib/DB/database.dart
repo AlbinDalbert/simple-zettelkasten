0658f92aa997081a25373b77c387a3c44fd7f1d1
Albin Dalbert <albin.dalbert.all@gmail.com>
2022-09-07 16:21:37 +0200

    more work on the database

--- lib/DB/database.dart
+++ lib/DB/database.dart
@@ -1,6 +1,7 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
+import 'db_model.dart';
 
 enum NoteType { fleeting, literature, permanent }
 
@@ -23,11 +24,53 @@ class NotesDatabase {
     return await openDatabase(path, version: 1, onCreate: _createDB);
   }
 
-  Future _createDB(Database db, int version) async {}
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const typeType = 'INTEGER NOT NULL';
+    const textType = 'TEXT NOT NULL';
+    const integerType = 'INTEGER NOT NULL';
+
+    await db.execute('''
+CREATE TABLE $tableNotes (
+  ${NoteFields.id} $idType,
+  ${NoteFields.type} $typeType,
+  ${NoteFields.title} $textType,
+  ${NoteFields.created} $textType,
+  ${NoteFields.modified} $textType,
+)
+''');
+  }
+
+  Future<Note> create(Note note) async {
+    final db = await instance.database;
+    final id = await db.insert(tableNotes, note.toJson());
+    return note.copy(id: id);
+  }
 
   Future close() async {
     final db = await instance.database;
 
     db.close();
   }
+
+  Future<Note> readNote(int id) async {
+    final db = await instance.database;
+
+    final res = await db.query(tableNotes,
+        columns: NoteFields.values,
+        where: '${NoteFields.id} = ?',
+        whereArgs: [id]);
+    if (res.isNotEmpty) {
+      return Note.fromJson(res.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future<List<Note>> readAllNotes() async {
+    final db = await instance.database;
+
+    final res = await db.query(tableNotes);
+
+    return res.map((json) => Note.fromJson(json)).toList();
 }
--- lib/DB/db_model.dart
+++ lib/DB/db_model.dart
@@ -3,13 +3,13 @@ import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:zettelkasten/Literature/writeLiterature.dart';
 
-enum NoteType { fleeting, literature, permanent }
+//enum NoteType { fleeting, literature, permanent }
 
-const String tableFleeting = 'fleeting';
-const String tableLiterature = 'literature';
-const String tablePermanent = 'permanent';
+const String tableNotes = 'notes';
 
 class NoteFields {
+  static final List<String> values = [id, type, title, created, modified];
+
   static const String id = '_id';
   static const String type = 'type';
   static const String title = 'title';
@@ -18,11 +18,11 @@ class NoteFields {
 }
 
 class Note {
-  final int id;
-  final NoteType type;
+  final int? id;
+  final int type;
   final String title;
-  final int created;
-  final int modified;
+  final DateTime created;
+  final DateTime modified;
 
   const Note(
       {required this.id,
@@ -30,4 +30,35 @@ class Note {
       required this.title,
       required this.created,
       required this.modified});
+
+  static Note fromJson(Map<String, Object?> json) => Note(
+        id: json[NoteFields.id] as int?,
+        type: json[NoteFields.type] as int,
+        title: json[NoteFields.title] as String,
+        created: DateTime.parse(json[NoteFields.created] as String),
+        modified: DateTime.parse(json[NoteFields.modified] as String),
+      );
+
+  Map<String, Object?> toJson() => {
+        NoteFields.id: id,
+        NoteFields.type: type,
+        NoteFields.title: title,
+        NoteFields.created: created.toIso8601String(),
+        NoteFields.modified: modified.toIso8601String(),
+      };
+
+  Note copy({
+    int? id,
+    int? type,
+    String? title,
+    DateTime? created,
+    DateTime? modified,
+  }) =>
+      Note(
+        id: id ?? this.id,
+        type: type ?? this.type,
+        title: title ?? this.title,
+        created: created ?? this.created,
+        modified: modified ?? this.modified,
+      );
 }
